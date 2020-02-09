249dae3bf7f1c36c33417f8e8568cffcec13f3eb
Micko Hakala <micko.hakala@tuni.fi>
2020-02-09 13:39:48 +0200

    PressTheButton start

---
+++ pressthebutton/services/pressCountService.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/presscount'
+
+const getInfo = async() => {
+  const response = await axios.get(baseUrl)
+  return response.data;
+}
+
+const pressButton = async(props) => {
+  const token = props.token
+  const res = getInfo();
+  const response = await axios.put(baseUrl, body)
+  return response.data
+}
---
+++ pressthebutton/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = (props) => {
+  return (
+    <div>
+      <p>ButtonGame</p>
+    </div>
+  )
+}
+
+export default App;
